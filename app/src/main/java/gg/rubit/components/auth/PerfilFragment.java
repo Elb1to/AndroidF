749efe2216e84574095b2add10f2b77f36a949d8
Elb1to <elbitomc@gmail.com>
2022-07-22 03:14:21 -0500

    More bs

--- app/src/main/java/gg/rubit/components/auth/PerfilFragment.java
+++ app/src/main/java/gg/rubit/components/auth/PerfilFragment.java
@@ -12,11 +12,8 @@ import gg.rubit.R;
 
 public class PerfilFragment extends Fragment {
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_notification, container, false);
     }
 }
