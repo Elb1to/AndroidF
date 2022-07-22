a43eaf618fab2bbc8e61cc3d2b5faa97ec553ddb
Elb1to <elbitomc@gmail.com>
2022-07-22 16:39:06 -0500

    Last changes to project

--- app/src/main/java/gg/rubit/components/ranking/RankingActivity.java
+++ app/src/main/java/gg/rubit/components/ranking/RankingActivity.java
@@ -1,16 +1,12 @@
 package gg.rubit.components.ranking;
 
 import android.graphics.drawable.AnimationDrawable;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import java.util.List;
 
@@ -30,20 +26,17 @@ public class RankingActivity extends AppCompatActivity {
     ImageView imgCargando;
     AnimationDrawable animationDrawable;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantallaranking);
 
-        imgCargando = (ImageView)findViewById(R.id.imgCargando);
+        imgCargando = findViewById(R.id.imgCargando);
         imgCargando.setBackgroundResource(R.drawable.cargando);
         imgCargando.setVisibility(View.VISIBLE);
 
-        animationDrawable = (AnimationDrawable)imgCargando.getBackground();
+        animationDrawable = (AnimationDrawable) imgCargando.getBackground();
         animationDrawable.start();
-
-
 
         InicializarControles();
         LoadListView(0);
@@ -54,9 +47,9 @@ public class RankingActivity extends AppCompatActivity {
         response.enqueue(new Callback<List<CVID_Tabla>>() {
             @Override
             public void onResponse(Call<List<CVID_Tabla>> call, Response<List<CVID_Tabla>> response) {
-                if (response.isSuccessful()){
+                if (response.isSuccessful()) {
                     List<CVID_Tabla> table = response.body();
-                    TablaListViewAdapter adapter = new TablaListViewAdapter(getApplicationContext(),table);
+                    TablaListViewAdapter adapter = new TablaListViewAdapter(getApplicationContext(), table);
                     imgCargando.setVisibility(View.GONE);
                     lstTabla.setVisibility(View.VISIBLE);
                     lstTabla.setAdapter(adapter);
@@ -71,20 +64,16 @@ public class RankingActivity extends AppCompatActivity {
     }
 
     private void InicializarControles() {
-        lstTabla = (ListView)findViewById(R.id.lstTabla);
+        lstTabla = findViewById(R.id.lstTabla);
     }
 
-    public void General(View v){
-
-        tipo=1;
+    public void General(View v) {
+        tipo = 1;
         LoadListView(tipo);
     }
 
-    public void Local(View v){
-
-        tipo=0;
+    public void Local(View v) {
+        tipo = 0;
         LoadListView(tipo);
     }
-
-
 }
